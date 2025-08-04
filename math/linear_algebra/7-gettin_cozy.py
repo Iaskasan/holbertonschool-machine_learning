d2e99334a01443a78a6f483e571a53be3b881bac
Iaskasan <nokiesan@yahoo.fr>
2025-08-04 15:41:03 +0200

    added cat matrices2d

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""Getting cozy"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """concatenate 2 2D matrices along a specific axis"""
+    if not isinstance(mat1, list) or not isinstance(mat2, list):
+        return None
+    if axis == 0:
+        return list(mat1 + mat2)
+    if axis == 1:
+        return [[*row1, *row2] for row1, row2 in zip(mat1, mat2)]
