2fb4d232cd082613f5e9a1a29e779a892003cb84
Iaskasan <nokiesan@yahoo.fr>
2025-08-04 15:50:38 +0200

    modified conditions

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,11 +4,11 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """concatenate 2 2D matrices along a specific axis"""
-    if not mat1 or not mat2:
-        return None
-    if len(mat1) == 0 or len(mat2) == 0:
-        return None
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         return list(mat1 + mat2)
     if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         return [[*row1, *row2] for row1, row2 in zip(mat1, mat2)]
