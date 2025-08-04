4795257d05562be56c722393838334e62d264e2b
Iaskasan <nokiesan@yahoo.fr>
2025-08-04 14:09:42 +0200

    removed numpy method and added list comprehension

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Adds two arrays element-wise"""
-import numpy as np
 
 
 def add_arrays(arr1, arr2):
     """Adds two arrays element-wise."""
     if len(arr1) != len(arr2):
         return None
-    return list(map(int, np.add(arr1, arr2)))
+    return [a + b for a, b in zip(arr1, arr2)]
