d3a4ae5c76bc28a4d21dcb2a6a65b9ecb86d1e4a
Iaskasan <nokiesan@yahoo.fr>
2025-08-04 13:25:46 +0200

    added doc and fixed pycodestyle

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 def matrix_shape(matrix):
+    """Returns the shape of a matrix as a list of dimensions."""
     shape = []
     while isinstance(matrix, list):
         shape.append(len(matrix))
