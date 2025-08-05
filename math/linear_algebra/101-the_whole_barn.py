a22fdfce092216d7f49c757d88eb3f02c8eb770f
Iaskasan <nokiesan@yahoo.fr>
2025-08-05 14:36:18 +0200

    added the matrices addition with same shape

--- math/linear_algebra/101-the_whole_barn.py
+++ math/linear_algebra/101-the_whole_barn.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""The whole barn"""
+import numpy as np
+
+
+def add_matrices(mat1, mat2):
+    """adds two matrices"""
+    array1 = np.array(mat1)
+    array2 = np.array(mat2)
+    if array1.shape != array2.shape:
+        return None
+    return (array1 + array2).tolist()
