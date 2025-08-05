b271a214cf54356856205a4c00714e1a894b3c56
Iaskasan <nokiesan@yahoo.fr>
2025-08-05 10:07:18 +0200

    added matrix mult

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""Ridin' Bareback"""
+
+
+def mat_mul(mat1, mat2):
+    """Performs matrix multiplication"""
+    if not mat1 or not mat2:
+        return None
+    if len(mat1[0]) != len(mat2):
+        return None
+    new_matrix = []
+    for row in mat1:
+        new_row = []
+        for j in range(len(mat2[0])):
+            dot_product = sum(row[k] * mat2[k][j] for k in range(len(mat2)))
+            new_row.append(dot_product)
+        new_matrix.append(new_row)
+    return new_matrix
