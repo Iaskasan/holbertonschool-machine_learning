bd43c581482f3842118755dbf458f22000199b73
Iaskasan <nokiesan@yahoo.fr>
2025-08-07 11:04:05 +0200

    added last advanced task

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -4,18 +4,50 @@
 
 def cat_matrices(mat1, mat2, axis=0):
     """concatenates two matrices along a specific axis"""
-    matrix = []
-    if len(mat1) != len(mat2):
+    shape1 = mat_shape(mat1)
+    shape2 = mat_shape(mat2)
+    if shape1 is None or shape2 is None:
         return None
+    if len(shape1) != len(shape2):
+        return None
+
     if axis == 0:
-        matrix = mat1 + mat2
-        return matrix
+        return mat1 + mat2
     if axis == 1:
-        matrix = [a+b for a, b in zip(mat1, mat2)]
-        return matrix
+        return [a+b for a, b in zip(mat1, mat2)]
     if axis == 2:
-        matrix = [a+b for a, b in zip(mat1[0], mat2[0])]
-        return matrix
+        new_mat = []
+        for i in range(len(mat1)):
+            axis2i = []
+            for j in range(len(mat1[i])):
+                axis2j = []
+                for k in range(len(mat1[i][j])):
+                    temp = mat1[i][j][k] + mat2[i][j][k]
+                    axis2j.append(temp)
+                axis2i.append(axis2j)
+            new_mat.append(axis2i)
+        return new_mat
     if axis == 3:
-        matrix = [a+b for a, b in zip(mat1[0][0], mat2[0][0])]
-        return matrix
+        new_matrix = []
+        for i in range(len(mat1)):
+            new_matrix_i = []
+            for j in range(len(mat1[i])):
+                new_matrix_j = []
+                for k in range(len(mat1[i][j])):
+                    new_list_k = mat1[i][j][k] + mat2[i][j][k]
+                    new_matrix_j.append(new_list_k)
+                new_matrix_i += new_matrix_j
+            new_matrix += new_matrix_i
+        return new_matrix
+
+
+def mat_shape(matrix):
+    """compare the shape of 2 matrices,
+    returns None if they dont match"""
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        if len(matrix) == 0:
+            break
+        matrix = matrix[0]
+    return shape
