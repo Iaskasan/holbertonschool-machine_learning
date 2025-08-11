12d4bf3084568ecdfa5310595cb73cba6cccc2a3
Iaskasan <nokiesan@yahoo.fr>
2025-08-11 10:18:42 +0200

    added some more words for documentation

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 import numpy as np
 import matplotlib.pyplot as plt
-"""Simple line"""
+"""Generate a Simple line"""
 
 
 def line():
-    """Generates a line graph"""
+    """Generate a red line graph"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     x = np.arange(0, 11)
