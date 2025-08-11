f57df65c0241d37dfa45f80390716819a27616e9
Iaskasan <nokiesan@yahoo.fr>
2025-08-11 11:49:41 +0200

    added 2nd task

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""Scatter: generate a scatter type graph"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def scatter():
+    """Generate a 2D scatter-typed graph"""
+    mean = [69, 0]
+    cov = [[15, 100], [100, 15]]
+    np.random.seed(5)
+    x, y = np.random.multivariate_normal(mean, cov, 2000).T
+    y += 600
+    plt.figure(figsize=(6.4, 4.8))
+    plt.scatter(x, y, color="magenta", s=10)
+    plt.title("Men's Height vs Weight")
+    plt.xlabel("Height (in)")
+    plt.ylabel("Weight (lbs)")
+    plt.show()
