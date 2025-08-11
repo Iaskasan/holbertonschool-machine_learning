00964581c30b640a335d0ea93c840df1137afafe
Iaskasan <nokiesan@yahoo.fr>
2025-08-11 10:20:38 +0200

    is that enough?

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
+"""Generate a Simple line"""
 import numpy as np
 import matplotlib.pyplot as plt
-"""Generate a Simple line"""
 
 
 def line():
-    """Generate a red line graph"""
+    """
+    Plots a red line graph of y = x^3 for x values from 0 to 10.
+
+    The function creates a figure and plots the cubic values
+    of integers from 0 to 10,
+    setting the x-axis limits from 0 to 10, and displays the plot.
+    """
+    """"""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     x = np.arange(0, 11)
