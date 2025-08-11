1b9c992dec235d52c671e62955f73d6193529a9e
Iaskasan <nokiesan@yahoo.fr>
2025-08-11 11:51:55 +0200

    forgot to remove some tests values oops

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -7,13 +7,15 @@ import matplotlib.pyplot as plt
 def scatter():
     """Generate a 2D scatter-typed graph"""
     mean = [69, 0]
-    cov = [[15, 100], [100, 15]]
+    cov = [[15, 8], [8, 15]]
     np.random.seed(5)
     x, y = np.random.multivariate_normal(mean, cov, 2000).T
-    y += 600
+    y += 180
     plt.figure(figsize=(6.4, 4.8))
-    plt.scatter(x, y, color="magenta")
+    plt.scatter(x, y, color="magenta", s=10)
     plt.title("Men's Height vs Weight")
     plt.xlabel("Height (in)")
     plt.ylabel("Weight (lbs)")
     plt.show()
+
+scatter()
