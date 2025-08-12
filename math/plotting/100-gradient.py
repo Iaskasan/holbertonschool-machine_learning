2d03f1c63f56ebd8e5680b9851a91ff37a9523ef
Iaskasan <nokiesan@yahoo.fr>
2025-08-12 14:44:05 +0200

    added advanced tasks files and coded first one

---
+++ math/plotting/100-gradient.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""Gradient, advanced task"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def gradient():
+    """create a scatter plot of sampled elevations on a mountain"""
+
+    np.random.seed(5)
+
+    x = np.random.randn(2000) * 10
+    y = np.random.randn(2000) * 10
+    z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
+    plt.figure(figsize=(6.4, 4.8))
+    plt.xlabel("x coordinate (m)")
+    plt.ylabel("y coordinate (m)")
+    plt.title("Mountain Elevation")
+    scatter = plt.scatter(x, y, c=z)
+    plt.colorbar(scatter)
+    plt.show()
