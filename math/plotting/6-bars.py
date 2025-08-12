66e9cde0f9be03ec725a1caabafd2b15a1a5bbc8
Iaskasan <nokiesan@yahoo.fr>
2025-08-12 11:48:22 +0200

    okay, first attempt of the stacked bar graph

---
+++ math/plotting/6-bars.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Stacking Bars task"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def bars():
+    """Generate a stacked bar graph"""
+    np.random.seed(5)
+    fruit = np.random.randint(0, 20, (4, 3))
+    apples = fruit[0]
+    bananas = fruit[1]
+    oranges = fruit[2]
+    peaches = fruit[3]
+    names = ["Farrah", "Fred", "Felicia"]
+    x = np.arange(0, 3, 1)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.ylabel("Quantity of Fruit")
+    plt.title("Number of Fruit per Person")
+    plt.yticks(np.arange(0, 81, 10))
+    plt.xticks(x, names)
+    plt.ylim(0, 80)
+    plt.bar(x, apples, color="red", width=0.5, label="Apples")
+    plt.bar(x, bananas, bottom=apples, color="yellow",
+            width=0.5, label="Bananas")
+    plt.bar(x, oranges, bottom=apples+bananas, color="orange",
+            width=0.5, label="Oranges")
+    plt.bar(x, peaches, bottom=apples+bananas+oranges, color="#ffe5b4",
+            width=0.5, label="Peaches")
+    plt.legend()
+    plt.show()
