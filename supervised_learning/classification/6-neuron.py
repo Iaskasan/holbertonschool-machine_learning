4e27ebeb02a39410ad80fb73b6076162bd2665f3
Iaskasan <nokiesan@yahoo.fr>
2025-09-03 12:44:24 +0200

    added train method

--- supervised_learning/classification/6-neuron.py
+++ supervised_learning/classification/6-neuron.py
@@ -65,3 +65,15 @@ class Neuron:
 
     def train(self, X, Y, iterations=5000, alpha=0.05):
         """train the neuron"""
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations < 1:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
+        for _ in range(iterations):
+            A = self.forward_prop(X)
+            self.gradient_descent(X, Y, A, alpha)
+        return self.evaluate(X, Y)
