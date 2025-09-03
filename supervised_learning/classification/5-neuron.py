63aeaf83e41c6e668552c318fab87ed7185a8143
Iaskasan <nokiesan@yahoo.fr>
2025-09-03 10:04:26 +0200

    corrected comment

--- supervised_learning/classification/5-neuron.py
+++ supervised_learning/classification/5-neuron.py
@@ -50,7 +50,7 @@ class Neuron:
     def evaluate(self, X, Y):
         """evaluate the neuron's prediction"""
         A = self.forward_prop(X)
-        # .astype transform booleans into int(O or 1)
+        # .astype transforms booleans into int(O or 1)
         pred = (A >= 0.5).astype(int)
         return pred, self.cost(Y, A)
 
