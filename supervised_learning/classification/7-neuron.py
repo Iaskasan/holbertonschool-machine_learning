5be2a6b31a82305a28f3fb3595b0a86368d7f3ea
Iaskasan <nokiesan@yahoo.fr>
2025-09-03 14:32:20 +0200

    added updated train method

---
+++ supervised_learning/classification/7-neuron.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+"""Neuron module"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class Neuron:
+    """Neuron class"""
+    def __init__(self, nx):
+        """
+        nx: number of input features to the neuron
+        __W: weights of the neuron
+        __b: bias
+        __A: activated output (prediction)
+        """
+        self.nx = nx
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
+
+    def forward_prop(self, X):
+        """Perform forward propagation of the neuron"""
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        "Calculate the cost of a model using logistic regression"
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        )
+        return cost
+
+    def evaluate(self, X, Y):
+        """evaluate the neuron's prediction"""
+        A = self.forward_prop(X)
+        # .astype transforms booleans into int(O or 1)
+        pred = (A >= 0.5).astype(int)
+        return pred, self.cost(Y, A)
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """calculate one pass of gradient descent on the neuron"""
+        m = Y.shape[1]
+        dZ = A - Y
+        dW = (dZ @ X.T) / m
+        db = np.sum(dZ) / m
+        self.__W = self.__W - alpha * dW
+        self.__b = self.__b - alpha * db
+
+    def train(self, X, Y, iterations=5000, alpha=0.05,
+              verbose=True, graph=True, step=100):
+        """train the neuron with plotting and verbose
+        during the training process"""
+        if type(iterations) is not int:
+            raise TypeError("iterations must be an integer")
+        if iterations < 1:
+            raise ValueError("iterations must be a positive integer")
+        if type(alpha) is not float:
+            raise TypeError("alpha must be a float")
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
+        if verbose or graph:
+            if type(step) is not int:
+                raise TypeError("step must be an integer")
+            if step <= 0 or step > iterations:
+                raise ValueError("step must be positive and <= iterations")
+        if graph:
+            plt.ion()
+            fig, ax = plt.subplots()
+            costs = []
+            iters = []
+
+        for i in range(iterations + 1):
+            A = self.forward_prop(X)
+            self.gradient_descent(X, Y, A, alpha)
+
+            if verbose and i % step == 0:
+                print(f"Cost after {i} iterations: {self.cost(Y, A)}")
+
+            if graph and (i % step == 0):
+                costs.append(self.cost(Y, A))
+                iters.append(i)
+                ax.clear()
+                ax.plot(iters, costs)
+                ax.set_xlabel("iteration")
+                ax.set_ylabel("cost")
+                ax.set_title("Training Cost")
+                plt.pause(0.01)
+        if graph:
+            plt.ioff()
+            plt.show()
+
+        return self.evaluate(X, Y)
