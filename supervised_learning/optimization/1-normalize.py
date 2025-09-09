835874f204fd262dc092ed843d64a1fd10a8bd22
Iaskasan <nokiesan@yahoo.fr>
2025-09-09 11:06:21 +0200

    added normalization of a dataset method

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""standardization of a matrix"""
+
+
+def normalize(X, m, s):
+    """
+    Normalizes (standardizes) a matrix
+
+    Args:
+        X (np.ndarray): shape (d, nx), data to normalize
+        m (np.ndarray): shape (nx,), mean of each feature
+        s (np.ndarray): shape (nx,), std of each feature
+
+    Returns:
+        np.ndarray: normalized matrix
+    """
+    return (X - m) / s
