ade36c28a66e5b54a16bd0f7cb4238be0eef7838
Iaskasan <nokiesan@yahoo.fr>
2025-09-09 10:53:25 +0200

    added normalization method

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+import numpy as np
+
+
+def normalization_constants(X):
+    """
+    Calculates the normalization (standardization) constants of a matrix
+
+    Args:
+        X (np.ndarray): shape (m, nx), the data to normalize
+
+    Returns:
+        mean (np.ndarray): shape (nx,), mean of each feature
+        std (np.ndarray): shape (nx,), standard deviation of each feature
+    """
+    mean = np.mean(X, axis=0)
+    std = np.std(X, axis=0)
+    return mean, std
