9b85acd04ecf856ecf4625708067a52f2dbec887
Iaskasan <nokiesan@yahoo.fr>
2025-09-09 14:23:36 +0200

    added RMSProp with keras

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""RMSProp with keras"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    Creates an RMSProp optimizer in TensorFlow.
+
+    Args:
+        alpha (float): learning rate
+        beta2 (float): RMSProp weight (discounting factor, usually 0.9)
+        epsilon (float): small constant to avoid division by zero
+
+    Returns:
+        tf.keras.optimizers.RMSprop: configured optimizer
+    """
+    return tf.keras.optimizers.RMSprop(
+        learning_rate=alpha,
+        rho=beta2,
+        epsilon=epsilon
+    )
