fdfad6a4613390a37f30e771982b3e14bd08f107
Iaskasan <nokiesan@yahoo.fr>
2025-09-09 13:49:42 +0200

    added gradient descent with momentum method

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+"""Gradient descent with momentum"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    Updates a variable using gradient descent with momentum.
+
+    Args:
+        alpha (float): learning rate
+        beta1 (float): momentum weight (0 < beta1 < 1)
+        var (np.ndarray): variable to be updated
+        grad (np.ndarray): gradient of var
+        v (np.ndarray): previous first moment (velocity)
+
+    Returns:
+        var (np.ndarray): updated variable
+        v (np.ndarray): new velocity (first moment)
+    """
+    v = beta1 * v + (1 - beta1) * grad
+    var = var - alpha * v
+    return var, v
