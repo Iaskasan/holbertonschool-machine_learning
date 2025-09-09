1d48778c757b5237b9c2da39a27ed37ee1d3ca81
Iaskasan <nokiesan@yahoo.fr>
2025-09-09 15:08:45 +0200

    added learning rate decay method

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -1,1 +1,18 @@
 #!/usr/bin/env python3
+"""time-based decay of alpha"""
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """
+    Updates the learning rate using inverse time decay.
+
+    Args:
+        alpha (float): initial learning rate
+        decay_rate (float): rate of decay
+        global_step (int): number of gradient descent steps elapsed
+        decay_step (int): number of steps before applying further decay
+
+    Returns:
+        float: updated learning rate
+    """
+    return alpha / (1 + decay_rate * (global_step // decay_step))
