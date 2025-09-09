ae4db62d7878f2c5c17e1cc756482c75ee9d22f0
Iaskasan <nokiesan@yahoo.fr>
2025-09-09 14:04:10 +0200

    added SGD method using keras

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""SGD with keras"""
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1):
+    """
+    Creates a gradient descent optimizer with momentum in TensorFlow.
+
+    Args:
+        alpha (float): learning rate
+        beta1 (float): momentum weight (0 < beta1 < 1)
+
+    Returns:
+        tf.keras.optimizers.Optimizer: momentum optimizer
+    """
+    return tf.keras.optimizers.SGD(learning_rate=alpha, momentum=beta1)
