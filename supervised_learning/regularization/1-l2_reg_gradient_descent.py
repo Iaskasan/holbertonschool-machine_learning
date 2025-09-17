f89c3ecc08ad85d967e0c864ed1183e2a1f1f6ee
Iaskasan <nokiesan@yahoo.fr>
2025-09-17 11:59:10 +0200

    trying to fix the output error (even tho its the same as checker)

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """L2 Regularization Gradient Descent"""
-
 import numpy as np
 
 
@@ -28,22 +27,16 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     -----
     Updates are done in place.
     """
-
     m = Y.shape[1]
+    dZ = cache['A' + str(L)] - Y
 
-    A_L = cache["A" + str(L)]
-    dZ = A_L - Y
-
-    for layer in range(L, 0, -1):
-        A_prev = cache["A" + str(layer - 1)]
-        W = weights["W" + str(layer)]
-
-        dW = (1 / m) * np.matmul(dZ, A_prev.T) + (lambtha / m) * W
+    for i in reversed(range(1, L + 1)):
+        A_prev = cache['A' + str(i - 1)]
+        W = weights['W' + str(i)]
+        dW = (1 / m) * (np.matmul(dZ, A_prev.T)) + (lambtha / m) * W
         db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
-
-        weights["W" + str(layer)] -= alpha * dW
-        weights["b" + str(layer)] -= alpha * db
-
-        if layer > 1:
-            A_prev = cache["A" + str(layer - 1)]
-            dZ = np.matmul(W.T, dZ) * (1 - A_prev**2)
+        weights['W' + str(i)] = W - alpha * dW
+        weights['b' + str(i)] = weights['b' + str(i)] - alpha * db
+        if i > 1:
+            A_prev = cache['A' + str(i - 1)]
+            dZ = np.matmul(W.T, dZ) * (1 - A_prev ** 2)
