21c75a338179a629aca85384fef2470fb8e7f0a3
Iaskasan <nokiesan@yahoo.fr>
2025-09-30 10:56:27 +0200

    moved the docstring

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import numpy as np
 """Performs forward propagation over a convolutional
     layer of a neural network"""
+import numpy as np
 
 
 def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
