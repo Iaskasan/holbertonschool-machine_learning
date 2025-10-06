64f58e88338d0b77f19ac10c9cbc5a2d0d5be5a5
Iaskasan <nokiesan@yahoo.fr>
2025-10-06 13:17:24 +0200

    changed import

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """builds an inception block"""
-import tensorflow as tf
+from tensorflow import keras as K
 
 
 def inception_block(A_prev, filters):
@@ -21,31 +21,31 @@ def inception_block(A_prev, filters):
         The concatenated output of the inception block.
     """
     F1, F3R, F3, F5R, F5, FPP = filters
-    conv_1x1 = tf.keras.layers.Conv2D(filters=F1, kernel_size=(1, 1),
+    conv_1x1 = K.layers.Conv2D(filters=F1, kernel_size=(1, 1),
                                       padding="same",
                                       activation="relu")(A_prev)
-    conv_3x3_reduce = tf.keras.layers.Conv2D(filters=F3R,
+    conv_3x3_reduce = K.layers.Conv2D(filters=F3R,
                                              kernel_size=(1, 1),
                                              padding='same',
                                              activation='relu')(A_prev)
-    conv_3x3 = tf.keras.layers.Conv2D(filters=F3,
+    conv_3x3 = K.layers.Conv2D(filters=F3,
                                       kernel_size=(3, 3),
                                       padding='same',
                                       activation='relu')(conv_3x3_reduce)
-    conv_5x5_reduce = tf.keras.layers.Conv2D(filters=F5R,
+    conv_5x5_reduce = K.layers.Conv2D(filters=F5R,
                                              kernel_size=(1, 1),
                                              padding='same',
                                              activation='relu')(A_prev)
-    conv_5x5 = tf.keras.layers.Conv2D(filters=F5, kernel_size=(5, 5),
+    conv_5x5 = K.layers.Conv2D(filters=F5, kernel_size=(5, 5),
                                       padding='same',
                                       activation='relu')(conv_5x5_reduce)
-    pool_proj = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=(1, 1),
+    pool_proj = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(1, 1),
                                              padding='same')(A_prev)
-    pool_proj = tf.keras.layers.Conv2D(filters=FPP,
+    pool_proj = K.layers.Conv2D(filters=FPP,
                                        kernel_size=(1, 1),
                                        padding='same',
                                        activation='relu')(pool_proj)
-    output = tf.keras.layers.Concatenate(axis=-1)([conv_1x1,
+    output = K.layers.Concatenate(axis=-1)([conv_1x1,
                                                    conv_3x3,
                                                    conv_5x5, pool_proj])
     return output
