99b0a0b25451d6cc5229a61a98cf1441d6041473
Iaskasan <nokiesan@yahoo.fr>
2025-10-09 14:30:37 +0200

    added dense block method

---
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""builds a transition layer as described in
+Densely Connected Convolutional Networks"""
+from tensorflow import keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    """"""
+    
