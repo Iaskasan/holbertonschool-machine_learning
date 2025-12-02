00106553b0ff4dbe6725d739252f06072bafaeb9
Iaskasan <nokiesan@yahoo.fr>
2025-12-02 14:42:11 +0100

    added cac matrix method

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+""" Concatenates two matrices"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """concatenates two matrices along a specific axis"""
+    if len(mat1) < 1 or len(mat2) < 1:
+        return None
+    new_mat = []
+    if axis == 0:
+        new_mat = mat1 + mat2
+        return new_mat
+    if axis == 1:
+        for i in range(len(mat1)):
+            new_mat.append(mat1[i] + mat2[i])
+        return new_mat
