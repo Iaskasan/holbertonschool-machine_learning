e5da5d1d4f5ab171d0e08e026c76d9a80c36c900
Iaskasan <nokiesan@yahoo.fr>
2025-12-02 14:44:38 +0100

    added shape check condition

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -8,9 +8,13 @@ def cat_matrices2D(mat1, mat2, axis=0):
         return None
     new_mat = []
     if axis == 0:
+        if len(mat1[0]) != len(mat2[0]):
+            return None
         new_mat = mat1 + mat2
         return new_mat
     if axis == 1:
+        if len(mat1) != len(mat2):
+            return None
         for i in range(len(mat1)):
             new_mat.append(mat1[i] + mat2[i])
         return new_mat
