9c5186a7ac54215b16125b026f616a66f001f788
Iaskasan <nokiesan@yahoo.fr>
2025-12-03 10:45:03 +0100

    added transpose and shape calculation methods

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+""" calculates the shape of an ndarray"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """calculates the shape of a numpy.ndarray"""
+    return np.shape(matrix)
--- math/linear_algebra/11-the_western_exchange.py
+++ math/linear_algebra/11-the_western_exchange.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Transposes a matrix"""
+
+
+def np_transpose(matrix):
+    """transposes a matrix"""
+    return matrix.T
