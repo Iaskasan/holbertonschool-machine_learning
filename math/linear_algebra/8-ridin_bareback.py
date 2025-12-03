b93b969fe2f56295356753708db4d05914fa6ca0
Iaskasan <nokiesan@yahoo.fr>
2025-12-03 10:06:44 +0100

    added matrix multiplication method

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""performs a matrix multiplication"""
+
+
+def mat_mul(mat1, mat2):
+    """performs matrix multiplication of two 2D matrices"""
+    if len(mat1) == 0 or len(mat2) == 0:
+        return None
+    if len(mat1[0]) != len(mat2):
+        return None
+    result = []
+    for i in range(len(mat1)):
+        new_row = []
+        for j in range(len(mat2[0])):
+            element = 0
+            for k in range(len(mat2)):
+                element += mat1[i][k] * mat2[k][j]
+            new_row.append(element)
+        result.append(new_row)
+    return result
