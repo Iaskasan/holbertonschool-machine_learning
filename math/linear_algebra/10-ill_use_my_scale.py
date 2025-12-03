25f8e52d7eb4d8347c47545d0f21bb99bf995769
Iaskasan <nokiesan@yahoo.fr>
2025-12-03 10:50:04 +0100

    removed numpy import

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """ calculates the shape of an ndarray"""
-import numpy as np
 
 
 def np_shape(matrix):
     """calculates the shape of a numpy.ndarray"""
-    return np.shape(matrix)
+    return matrix.shape
