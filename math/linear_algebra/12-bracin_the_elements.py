3a846b37180d26e7d0315ce4456822faebbfd6df
Iaskasan <nokiesan@yahoo.fr>
2025-12-03 11:16:48 +0100

    added element-wise multiplication method

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -5,4 +5,4 @@
 def np_elementwise(mat1, mat2):
     """performs element-wise addition, subtraction,
     multiplication, and division"""
-    return (mat1 + mat2, mat1)
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
