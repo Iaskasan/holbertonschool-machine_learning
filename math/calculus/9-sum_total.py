3e48a95b5902c40d3d7facc7ccb74eba767acfd0
Iaskasan <nokiesan@yahoo.fr>
2025-12-09 11:10:11 +0100

    added the summation function

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""sum function"""
+
+
+def summation_i_squared(n):
+    """calculates the summation of i squared
+    with n as a stopping condition"""
+    sum = 0
+    for i in range(1, n + 1):
+        sum += i**2
+    return sum
