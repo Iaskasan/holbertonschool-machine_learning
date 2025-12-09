ad7e9ee887061f3c1c7fd65509ddacb10ea2312b
Iaskasan <nokiesan@yahoo.fr>
2025-12-09 13:07:12 +0100

    added the famous squared summation method!

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,6 +5,6 @@
 def summation_i_squared(n):
     """calculates the summation of i squared
     with n as a stopping condition"""
-    if type(n) is not int:
+    if type(n) is not int or n < 1:
         return None
-    return (n**2) * 2 + n
+    return ((n * (n + 1)) * (2 * n + 1)) / 6
