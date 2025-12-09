fd26b4fee7405ca5d3036572a2111d2b82aab8fa
Iaskasan <nokiesan@yahoo.fr>
2025-12-09 13:25:28 +0100

    added derivative of poly method

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+"""Calculates the derivative of a polynomial"""
+
+
+def poly_derivative(poly):
+    """Outputs a list of the derivatives
+    of a given polynomial list"""
+    if type(poly) is not list:
+        return None
+    result = []
+    for i in range(len(poly)):
+        if i == 0:
+            continue
+        result.append(poly[i] * i)
+    return result
