af0d0b8b2e438137e1085201be554f2e8cc552cc
Iaskasan <nokiesan@yahoo.fr>
2025-12-09 11:36:14 +0100

    trying this calculation

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,7 +5,6 @@
 def summation_i_squared(n):
     """calculates the summation of i squared
     with n as a stopping condition"""
-    sum = 0
-    for i in range(1, n + 1):
-        sum += i**2
-    return sum
+    if type(n) is not int:
+        return None
+    return (n**2) * 2 + n
