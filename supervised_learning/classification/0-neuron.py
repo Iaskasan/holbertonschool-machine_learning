15d4930a7c988aee23bc3bde84b8b9bd5d7ec74b
Iaskasan <nokiesan@yahoo.fr>
2026-01-06 13:28:32 +0100

    fixed random initialisation

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -18,6 +18,6 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         self.nx = nx
-        self.W = np.random.normal(0, nx)
+        self.W = np.random.normal()
         self.b = 0
         self.A = 0
