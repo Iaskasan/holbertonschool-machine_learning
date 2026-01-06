e02f8a8cee6e8aa42062d89698b80ea9f01d05c5
Iaskasan <nokiesan@yahoo.fr>
2026-01-06 13:30:40 +0100

    changed initialisation of weights' shape

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -18,6 +18,6 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
         self.nx = nx
-        self.W = np.random.normal()
+        self.W = np.random.normal((1, nx))
         self.b = 0
         self.A = 0
