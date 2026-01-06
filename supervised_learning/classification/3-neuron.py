c85dc70193beda2c2fbee3783d88713ef092c5f9
Iaskasan <nokiesan@yahoo.fr>
2026-01-06 15:13:27 +0100

    added the cost method

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+"""Clas neuron module"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron class that defines a single
+    neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Constructor method
+
+        Args:
+            nx (int): number of input features to the neuron
+            W (numpy.ndarray): weights vector
+            b (float): bias value
+            A (float): activated output
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Weights getter
+
+        Returns:
+            numpy.ndarray: weights vector
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """Bias getter
+
+        Returns:
+            float: bias value
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """Activated output getter
+
+        Returns:
+            float: activated output
+        """
+        return self.__A
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neuron
+
+        Args:
+            X (numpy.ndarray): input data of shape (nx, m)
+
+        Returns:
+            numpy.ndarray: activated output of the neuron
+        """
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression
+        Args:
+            Y (numpy.ndarray): correct labels of shape (1, m)
+            A (numpy.ndarray): activated output of shape (1, m)
+        Returns:
+            float: cost of the model
+        """
+        m = Y.shape[1]
+        cost = - (1 / m) * np.sum(Y * np.log(A) +
+                                  (1 - Y) * np.log(1.0000001 - A))
+        return cost
