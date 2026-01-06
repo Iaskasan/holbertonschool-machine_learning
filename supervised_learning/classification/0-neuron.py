c8d7b1338c38c0c549ec58575af38c4a9b42c11d
Iaskasan <nokiesan@yahoo.fr>
2026-01-06 13:19:23 +0100

    added neuron class with constructor

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Clas neuron module"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron class that defines a single
+    neuron performing binary classification"""
+
+    def __init__(self, nx):
+        """Constructor method
+
+        Args:
+            nx (int): number of input features to the neuron
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.nx = nx
+        self.W = np.random.normal(0, nx)
+        self.b = 0
+        self.A = 0
