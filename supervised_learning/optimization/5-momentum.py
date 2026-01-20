e24dd9c5ca4431549a55443e83479aacdc8021c4
Iaskasan <nokiesan@yahoo.fr>
2026-01-20 15:46:56 +0100

    added the weighted moving average method

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python3
+"""calculates the weighted moving average of a data set"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def moving_average(data, beta):
+    """calculates moving average of a dataset
+    Args:
+        data (list): data to calculate the moving average of
+        beta (float): weight used for the moving average
+    Returns:
+        list: moving averages of data
+    """
+    ema = []
+    ema.append(data[0])
+    for i in range(1, len(data)):
+        ema.append(beta * ema[i - 1] + (1 - beta) * data[i])
+    return ema
