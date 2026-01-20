b3a333b02ea8026ba8063273423f0d1a0d2a5b76
Iaskasan <nokiesan@yahoo.fr>
2026-01-20 14:16:57 +0100

    added the normalization method

--- supervised_learning/optimization/1-normalize.py
+++ supervised_learning/optimization/1-normalize.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""normalize dataset using provided normalization constants"""
+import numpy as np
+
+
+def normalize(X, m, s):
+    """
+    Normalize dataset X using mean m and std s.
+    """
+    X_norm = (X - m) / s
+    return X_norm
