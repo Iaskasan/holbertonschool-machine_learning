9b16c054b72fac56ec23f3f6d63edd80269b6d0d
Iaskasan <nokiesan@yahoo.fr>
2026-01-20 14:10:10 +0100

    added the std normalization + mean calculation method

--- supervised_learning/optimization/0-norm_constants.py
+++ supervised_learning/optimization/0-norm_constants.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+import numpy as np
+
+
+def normalization_constants(X):
+    """
+    Compute normalization constants for each feature in dataset X.
+    """
+    mean = np.sum(X, axis=0) / X.shape[0]
+    std = np.sqrt(np.sum(((X - mean) ** 2 / X.shape[0]), axis=0))
+    return mean, std
