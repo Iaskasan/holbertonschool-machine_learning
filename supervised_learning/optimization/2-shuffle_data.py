83a325dc5413b60a7b73ed20a18aea794c4ee6d2
Iaskasan <nokiesan@yahoo.fr>
2026-01-20 14:34:59 +0100

    method that shuffles 2 arrays the same way

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+"""shuffle dataset X
+"""
+import numpy as np
+
+
+def shuffle_data(X, Y):
+    """shuffles data points in X and Y in the same way
+    """
+    X_shuffle = np.random.permutation(X.shape[0])
+    return X[X_shuffle], Y[X_shuffle]
