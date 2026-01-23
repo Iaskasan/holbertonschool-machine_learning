0d880d0811ea3f1bfb3ee319753bc4753514cfdc
Iaskasan <nokiesan@yahoo.fr>
2026-01-23 09:41:47 +0100

    added momentum opt algo with tf.keras

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+"""sets up the gradient descent
+with momentum optimization algorithm
+"""
+import tensorflow as tf
+
+
+def create_momentum_op(alpha, beta1):
+    """sets up the gradient descent
+    with momentum optimization algorithm"""
+    optimizer = tf.keras.optimizers.SGD(alpha, beta1)
+    return optimizer
