487f7f7bef6432aa80feca5163f08edda5a80da3
Iaskasan <nokiesan@yahoo.fr>
2026-01-23 11:21:50 +0100

    added batch norm before activation

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -5,11 +5,21 @@ import tensorflow as tf
 
 
 def create_batch_norm_layer(prev, n, activation):
-    """Creates a batch normalization layer for a neural network."""
+    """
+    Creates a batch normalization layer with BN applied BEFORE activation.
+
+    Args:
+        prev (tf.Tensor): Output of previous layer (can be already activated).
+        n (int): Number of nodes in the new layer.
+        activation (callable or str): Activation function to apply.
+
+    Returns:
+        tf.Tensor: Activated output of the batch-normalized layer.
+    """
     initializer = tf.keras.initializers.VarianceScaling(mode='fan_avg')
     dense = tf.keras.layers.Dense(
         units=n,
-        activation=activation,
+        activation=None,
         kernel_initializer=initializer
     )(prev)
 
@@ -19,4 +29,9 @@ def create_batch_norm_layer(prev, n, activation):
         beta_initializer=tf.keras.initializers.Zeros()
     )(dense)
 
-    return batch_norm
+    if activation is not None:
+        output = tf.keras.layers.Activation(activation)(batch_norm)
+    else:
+        output = batch_norm
+
+    return output
