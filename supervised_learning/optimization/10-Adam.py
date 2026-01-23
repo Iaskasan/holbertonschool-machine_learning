694707035132309d3436b4cf2b9be26d50b882e9
Iaskasan <nokiesan@yahoo.fr>
2026-01-23 10:11:44 +0100

    added adam optimizer method using tensorflow

--- supervised_learning/optimization/10-Adam.py
+++ supervised_learning/optimization/10-Adam.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""sets up the Adam optimization algorithm"""
+import tensorflow as tf
+
+
+def create_Adam_op(alpha, beta1, beta2, epsilon):
+    """sets up the Adam optimization algorithm
+    Args:
+        alpha (float): learning rate
+        beta1 (float): momentum weight
+        beta2 (float): RMSProp weight
+        epsilon (float): small number to avoid division by zero
+    Returns:
+        tf.train.Optimizer: Adam optimizer
+    """
+    optimizer = tf.keras.optimizers.Adam(learning_rate=alpha,
+                                         beta_1=beta1,
+                                         beta_2=beta2,
+                                         epsilon=epsilon)
+    return optimizer
