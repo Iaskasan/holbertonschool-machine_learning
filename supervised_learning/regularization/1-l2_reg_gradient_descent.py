7855cbc9e629d4b87fd780efac7f3bfc859533b7
Iaskasan <nokiesan@yahoo.fr>
2026-01-28 15:26:58 +0100

    added the gradient descent pass method using l2 regularization

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+"""
+updates the weights and biases of a neural
+network using gradient descent with L2 regularization
+"""
+import numpy as np
+
+
+def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
+    """
+    Updates the weights and biases of a neural network using
+    gradient descent with L2 regularization
+
+    Args:
+        Y: numpy.ndarray - one-hot numpy.ndarray of shape (classes, m)
+                           with correct labels
+        weights: dict - weights and biases (keys 'W1', 'b1', ...)
+        cache: dict - outputs of each layer (keys 'A0', 'A1', ...)
+        alpha: float - learning rate
+        lambtha: float - regularization parameter
+        L: int - number of layers
+
+    Returns:
+        None - updates weights in place
+    """
+    m = Y.shape[1]
+    dZ = cache['A' + str(L)] - Y
+
+    for i in range(L, 0, -1):
+        A_prev = cache['A' + str(i - 1)]
+        W = weights['W' + str(i)]
+
+        dW = (1 / m) * np.dot(dZ, A_prev.T) + (lambtha / m) * W
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+        weights['W' + str(i)] -= alpha * dW
+        weights['b' + str(i)] -= alpha * db
+
+        if i > 1:
+            dZ = np.dot(W.T, dZ) * (A_prev * (1 - A_prev))
