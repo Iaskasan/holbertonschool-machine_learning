5db3dfa66632d191addeab4f5f36378ba18a4bfd
Iaskasan <nokiesan@yahoo.fr>
2026-01-28 13:52:08 +0100

    used previous method precision and sensitivity (recall) instead of hardcoding them

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """calculates F1 score for a confusion matrix"""
 import numpy as np
+sensitivity = __import__('1-sensitivity').sensitivity
+precision = __import__('2-precision').precision
 
 
 def f1_score(confusion):
@@ -14,21 +16,9 @@ def f1_score(confusion):
     Returns:
     np.ndarray of shape (classes,) - F1 score for each class
     """
-    true_positives = np.diag(confusion)
-    false_positives = np.sum(confusion, axis=0) - true_positives
-    false_negatives = np.sum(confusion, axis=1) - true_positives
-
     with np.errstate(divide='ignore', invalid='ignore'):
-        precision_values = true_positives / (
-            true_positives + false_positives)
-        precision_values = np.nan_to_num(precision_values)
-
-        sensitivity_values = true_positives / (
-            true_positives + false_negatives)
-        sensitivity_values = np.nan_to_num(sensitivity_values)
-
-        f1_scores = 2 * (precision_values * sensitivity_values) / (
-            precision_values + sensitivity_values)
+        f1_scores = 2 * precision(confusion) * sensitivity(confusion) / (
+            precision(confusion) + sensitivity(confusion))
         f1_scores = np.nan_to_num(f1_scores)
 
     return f1_scores
