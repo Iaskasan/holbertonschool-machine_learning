340503ef0e964a45e8d42995773e0a244a385b79
Iaskasan <nokiesan@yahoo.fr>
2026-01-28 15:34:45 +0100

    fixed activation functions

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -24,17 +24,18 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         None - updates weights in place
     """
     m = Y.shape[1]
-    dZ = cache['A' + str(L)] - Y
+    dZ = cache["A" + str(L)] - Y
 
-    for i in range(L, 0, -1):
-        A_prev = cache['A' + str(i - 1)]
-        W = weights['W' + str(i)]
+    for i in reversed(range(1, L + 1)):
+        A_prev = cache["A" + str(i - 1)]
+        W = weights["W" + str(i)]
+        b = weights["b" + str(i)]
 
-        dW = (1 / m) * np.dot(dZ, A_prev.T) + (lambtha / m) * W
-        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        dW = (1 / m) * (dZ @ A_prev.T) + (lambtha / m) * W
+        db = (1 / m) * dZ.sum(axis=1, keepdims=True)
 
-        weights['W' + str(i)] -= alpha * dW
-        weights['b' + str(i)] -= alpha * db
-
+        weights["W" + str(i)] = W - alpha * dW
+        weights["b" + str(i)] = b - alpha * db
         if i > 1:
-            dZ = np.dot(W.T, dZ) * (A_prev * (1 - A_prev))
+            A_prev = cache["A" + str(i - 1)]
+            dZ = (W.T @ dZ) * (1 - A_prev ** 2)
