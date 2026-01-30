7764b62723d465121752653d19e4831d7e6dabb8
Iaskasan <nokiesan@yahoo.fr>
2026-01-30 09:45:39 +0100

    added the method that creates a dense layer using l2 regularzation

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,1 +1,29 @@
 #!/usr/bin/env python3
+"""
+creates a neural network layer in
+tensorFlow that includes L2 regularization
+"""
+import tensorflow as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """
+    Creates a dense layer with L2 regularization
+
+    Args:
+        prev: tensor, output of the previous layer
+        n: int, number of nodes in the layer to create
+        activation: activation function for the layer
+        lambtha: float, L2 regularization parameter
+
+    Returns:
+        tensor, output of the layer
+    """
+    regularizer = tf.keras.regularizers.L2(l2=lambtha)
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_regularizer=regularizer,
+        bias_regularizer=regularizer
+    )
+    return layer(prev)
