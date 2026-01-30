6c1a60156219ec6ca1e801aa39e19b9234efa5cd
Iaskasan <nokiesan@yahoo.fr>
2026-01-30 09:26:48 +0100

    added L2 reg cost method with tensorflow

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""calculates the cost of a neural network with L2 regularization"""
+import tensorflow as tf
+
+
+def l2_reg_cost(cost, model):
+    """
+    Calculates total cost with L2 regularization for a Keras model
+
+    Args:
+        cost: tensor, cost without L2 regularization
+        model: keras model with L2 regularization
+
+    Returns:
+        tensor, total cost including L2 regularization
+    """
+    return cost + sum(model.losses)
