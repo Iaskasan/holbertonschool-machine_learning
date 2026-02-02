24be61c3d02b7dd625c07a93502a42dd081e6d45
Iaskasan <nokiesan@yahoo.fr>
2026-02-02 12:32:23 +0100

    added convolution on grayscale images method using same conv

--- math/convolutions_and_pooling/1-convolve_grayscale_same.py
+++ math/convolutions_and_pooling/1-convolve_grayscale_same.py
@@ -1,1 +1,40 @@
 #!/usr/bin/env python3
+"""performs a same convolution on grayscale images"""
+import numpy as np
+
+
+def convolve_grayscale_same(images, kernel):
+    """
+    Performs a same convolution on grayscale images.
+
+    images: numpy.ndarray of shape (m, h, w)
+    kernel: numpy.ndarray of shape (kh, kw)
+
+    Returns: numpy.ndarray of shape (m, h, w)
+    """
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+
+    pad_h = kh - 1
+    pad_w = kw - 1
+
+    pad_top = pad_h // 2
+    pad_bottom = pad_h - pad_top
+    pad_left = pad_w // 2
+    pad_right = pad_w - pad_left
+
+    images_padded = np.pad(
+        images,
+        ((0, 0), (pad_top, pad_bottom), (pad_left, pad_right)),
+        mode='constant',
+        constant_values=0
+    )
+
+    output = np.zeros((m, h, w))
+
+    for i in range(h):
+        for j in range(w):
+            patch = images_padded[:, i:i + kh, j:j + kw]
+            output[:, i, j] = np.sum(patch * kernel, axis=(1, 2))
+
+    return output
