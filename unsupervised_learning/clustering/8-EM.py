ce30f1cf9f6270f776977ee7085e6ee7ffc4b9a5
Iaskasan <nokiesan@yahoo.fr>
2026-04-10 11:35:55 +0200

    refactor: streamline expectation_maximization function and enhance docstring clarity

--- unsupervised_learning/clustering/8-EM.py
+++ unsupervised_learning/clustering/8-EM.py
@@ -10,18 +10,6 @@ maximization = __import__('7-maximization').maximization
 def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     """
     Performs the expectation maximization for a GMM.
-
-    Args:
-        X: numpy.ndarray of shape (n, d) containing the data set
-        k: positive integer containing the number of clusters
-        iterations: positive integer containing the maximum number of
-                    iterations for the algorithm
-        tol: non-negative float containing tolerance of the log likelihood
-        verbose: boolean that determines if information should be printed
-
-    Returns:
-        pi, m, S, g, l
-        or None, None, None, None, None on failure
     """
     if not isinstance(X, np.ndarray) or X.ndim != 2:
         return None, None, None, None, None
@@ -38,20 +26,26 @@ def expectation_maximization(X, k, iterations=1000, tol=1e-5, verbose=False):
     if pi is None or m is None or S is None:
         return None, None, None, None, None
 
-    prev_l = None
+    prev_l = 0
 
     for i in range(iterations + 1):
         g, log = expectation(X, pi, m, S)
-        if g is None or log is None:
+        if g is None or l is None:
             return None, None, None, None, None
 
-        if verbose and (i % 10 == 0 or i == iterations):
+        if verbose and (i % 10 == 0):
             print("Log Likelihood after {} iterations: {:.5f}".format(i, log))
 
-        if prev_l is not None and abs(log - prev_l) <= tol:
+        if i != 0 and abs(log - prev_l) <= tol:
+            if verbose and (i % 10 != 0):
+                print("Log Likelihood after {} iterations: {:.5f}"
+                      .format(i, log))
             return pi, m, S, g, log
 
         if i == iterations:
+            if verbose and (i % 10 != 0):
+                print("Log Likelihood after {} iterations: {:.5f}"
+                      .format(i, log))
             return pi, m, S, g, log
 
         pi, m, S = maximization(X, g)
