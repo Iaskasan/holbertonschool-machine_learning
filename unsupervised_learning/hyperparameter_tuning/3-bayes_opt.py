1c623ebfa113ed4393c9db9d45207903dbeb596c
Iaskasan <nokiesan@yahoo.fr>
2026-04-17 09:57:33 +0200

    Creation and initialization of the BayesianOptimization class

--- unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/3-bayes_opt.py
@@ -1,1 +1,35 @@
 #!/usr/bin/env python3
+"""Bayesian optimization with Gaussian Processes"""
+import numpy as np
+GP = __import__('2-gp').GaussianProcess
+
+
+class BayesianOptimization():
+    """Performs Bayesian optimization on a noiseless 1D Gaussian process"""
+
+    def __init__(self, f, X_init, Y_init, bounds, ac_samples,
+                 l=1, sigma_f=1, xsi=0.01, minimize=True):
+        """Class constructor
+
+        Args:
+            f (function): The black-box function to be optimized
+            X_init (numpy.ndarray): Array of shape (t, 1) representing the
+        inputs already sampled with the black-box function
+            Y_init (numpy.ndarray): Array of shape (t, 1) representing the
+        outputs of the black-box function for each input in X_init
+            bounds (tuple): Tuple of (min, max) representing the bounds of the
+        space in which to look for the optimal point
+            ac_samples (int): The number of samples that should be analyzed
+        during acquisition
+            l (float): The length parameter for the kernel
+            sigma_f (float): The standard deviation given to the output of the
+            kernel
+            xsi (float): The exploration-exploitation factor for acquisition
+            minimize (bool): A bool determining whether optimization should be
+        performed for minimization (True) or maximization (False)
+        """
+        self.f = f
+        self.gp = GP(X_init, Y_init, l, sigma_f)
+        self.xsi = xsi
+        self.minimize = minimize
+        self.X_s = np.linspace(bounds[0], bounds[1], ac_samples).reshape(-1, 1)
