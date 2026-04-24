f66c40db419e339670659e26861519d4b0e300bf
Iaskasan <nokiesan@yahoo.fr>
2026-04-24 10:14:02 +0200

    added the vanilla autoencoder method

--- unsupervised_learning/autoencoders/0-vanilla.py
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -1,1 +1,58 @@
 #!/usr/bin/env python3
+"""Vanilla autoencoder module."""
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    """
+    Creates a vanilla autoencoder.
+
+    Args:
+        input_dims: Integer, dimensions of the model input.
+        hidden_layers: List of integers, number of nodes for each
+            hidden layer in the encoder.
+        latent_dims: Integer, dimensions of the latent space.
+
+    Returns:
+        encoder: The encoder model.
+        decoder: The decoder model.
+        auto: The full autoencoder model.
+    """
+    # Encoder
+    encoder_input = keras.Input(shape=(input_dims,))
+    x = encoder_input
+
+    for nodes in hidden_layers:
+        x = keras.layers.Dense(nodes, activation="relu")(x)
+
+    latent = keras.layers.Dense(latent_dims, activation="relu")(x)
+
+    encoder = keras.Model(encoder_input, latent, name="encoder")
+
+    # Decoder
+    decoder_input = keras.Input(shape=(latent_dims,))
+    x = decoder_input
+
+    for nodes in reversed(hidden_layers):
+        x = keras.layers.Dense(nodes, activation="relu")(x)
+
+    decoder_output = keras.layers.Dense(
+        input_dims,
+        activation="sigmoid"
+    )(x)
+
+    decoder = keras.Model(decoder_input, decoder_output, name="decoder")
+
+    # Full autoencoder
+    auto_input = keras.Input(shape=(input_dims,))
+    encoded = encoder(auto_input)
+    decoded = decoder(encoded)
+
+    auto = keras.Model(auto_input, decoded, name="autoencoder")
+
+    auto.compile(
+        optimizer="adam",
+        loss="binary_crossentropy"
+    )
+
+    return encoder, decoder, auto
