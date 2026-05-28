1216b7f7228121ae323f3756a17df13d7416c147
Iaskasan <nokiesan@yahoo.fr>
2026-05-28 11:29:36 +0200

    add: implement RNN decoder with attention mechanism for machine translation

---
+++ supervised_learning/attention/1-self_attention.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Self-attention layer for machine translation."""
+
+import tensorflow as tf
+
+
+class SelfAttention(tf.keras.layers.Layer):
+    """Calculates the attention context and weights."""
+
+    def __init__(self, units):
+        """Set up the alignment model layers."""
+        super().__init__()
+        self.W = tf.keras.layers.Dense(units)
+        self.U = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, s_prev, hidden_states):
+        """Compute the context vector and attention weights."""
+        s_prev = tf.expand_dims(s_prev, 1)
+        score = self.V(tf.nn.tanh(self.W(s_prev) + self.U(hidden_states)))
+        weights = tf.nn.softmax(score, axis=1)
+        context = tf.reduce_sum(weights * hidden_states, axis=1)
+        return context, weights
---
+++ supervised_learning/attention/2-rnn_decoder.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""RNN decoder for machine translation."""
+
+import tensorflow as tf
+
+SelfAttention = __import__('1-self_attention').SelfAttention
+
+
+class RNNDecoder(tf.keras.layers.Layer):
+    """Decodes a target token using attention over encoder outputs."""
+
+    def __init__(self, vocab, embedding, units, batch):
+        """Set up the decoder layers."""
+        super().__init__()
+        self.embedding = tf.keras.layers.Embedding(vocab, embedding)
+        self.gru = tf.keras.layers.GRU(
+            units,
+            return_sequences=True,
+            return_state=True,
+            recurrent_initializer='glorot_uniform'
+        )
+        self.F = tf.keras.layers.Dense(vocab)
+        self.attention = SelfAttention(units)
+
+    def call(self, x, s_prev, hidden_states):
+        """Decode one token step from the previous state and encoder states."""
+        context, _ = self.attention(s_prev, hidden_states)
+        x = self.embedding(x)
+        context = tf.expand_dims(context, 1)
+        x = tf.concat([context, x], axis=-1)
+        output, s = self.gru(x, initial_state=s_prev)
+        y = self.F(tf.squeeze(output, axis=1))
+        return y, s
