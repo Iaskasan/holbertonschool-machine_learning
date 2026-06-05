b17fbd101ba1117941388922655c82f22766e13c
Iaskasan <nokiesan@yahoo.fr>
2026-06-05 10:31:33 +0200

    add: implement dataset loader and tokenizer for translation

---
+++ supervised_learning/transformer_apps/0-dataset.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""Dataset loader and tokenizer preparation for translation."""
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+from transformers import AutoTokenizer
+
+
+class Dataset:
+    """Load the translation dataset and prepare tokenizers."""
+
+    def __init__(self):
+        """Load dataset splits and build tokenizers from the train split."""
+        self.data_train, self.data_valid = tfds.load(
+            "ted_hrlr_translate/pt_to_en",
+            split=["train", "validation"],
+            as_supervised=True
+        )
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(
+            self.data_train
+        )
+
+    def tokenize_dataset(self, data):
+        """Create Portuguese and English sub-word tokenizers."""
+        tokenizer_pt = AutoTokenizer.from_pretrained(
+            "neuralmind/bert-base-portuguese-cased",
+            use_fast=True
+        )
+        tokenizer_en = AutoTokenizer.from_pretrained(
+            "bert-base-uncased",
+            use_fast=True
+        )
+
+        def iterator(lang_index, batch_size=1024):
+            for batch in data.batch(batch_size):
+                texts = batch[lang_index].numpy()
+                yield [text.decode("utf-8") for text in texts]
+
+        cardinality = tf.data.experimental.cardinality(data).numpy()
+        length = None if cardinality < 0 else int(cardinality)
+
+        tokenizer_pt = tokenizer_pt.train_new_from_iterator(
+            iterator(0),
+            vocab_size=2 ** 13,
+            length=length
+        )
+        tokenizer_en = tokenizer_en.train_new_from_iterator(
+            iterator(1),
+            vocab_size=2 ** 13,
+            length=length
+        )
+
+        return tokenizer_pt, tokenizer_en
