3b5151f9c6d7bd5bf4a84870c320b53cb63e3d87
Iaskasan <nokiesan@yahoo.fr>
2026-06-19 10:21:01 +0200

    refactor: simplify FrozenLake environment loading by removing random map generation

--- reinforcement_learning/q_learning/0-load_env.py
+++ reinforcement_learning/q_learning/0-load_env.py
@@ -2,16 +2,10 @@
 """Load the FrozenLake environment."""
 
 import gymnasium as gym
-from gymnasium.envs.toy_text.frozen_lake import generate_random_map
 
 
 def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
     """Load a FrozenLake environment."""
-    if desc is None and map_name is None:
-        desc = generate_random_map(size=8)
-    elif desc is not None:
-        desc = ["".join(row) if isinstance(row, list) else row for row in desc]
-
     return gym.make(
         "FrozenLake-v1",
         desc=desc,
