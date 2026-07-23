00dd64b9dc929cf4804c01e43920d4d52422071e
Iaskasan <nokiesan@yahoo.fr>
2026-07-23 11:23:08 +0200

    add: implement array function to return last 10 rows of High and Close columns as a NumPy array

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Converts selected DataFrame values into a NumPy array."""
+
+
+def array(df):
+    """Return the last 10 rows of the High and Close columns as an array."""
+    return df[["High", "Close"]].tail(10).to_numpy()
