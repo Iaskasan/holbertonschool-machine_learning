7590777362dd900b5df3295207c13effeff8e6ed
Iaskasan <nokiesan@yahoo.fr>
2026-07-23 14:34:25 +0200

    add: implement hierarchy function to concatenate market data with MultiIndex

--- pipeline/pandas/12-hierarchy.py
+++ pipeline/pandas/12-hierarchy.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python3
+"""Concatenates market data using a chronological MultiIndex."""
+import pandas as pd
+
+
+index = __import__('10-index').index
+
+
+def hierarchy(df1, df2):
+    """Combine coinbase and bitstamp data within the timestamp interval."""
+    df1 = index(df1)
+    df2 = index(df2)
+
+    start = 1417411980
+    end = 1417417980
+    df1 = df1[(df1.index >= start) & (df1.index <= end)]
+    df2 = df2[(df2.index >= start) & (df2.index <= end)]
+
+    combined = pd.concat([df2, df1], keys=["bitstamp", "coinbase"])
+    return combined.swaplevel(0, 1).sort_index()
