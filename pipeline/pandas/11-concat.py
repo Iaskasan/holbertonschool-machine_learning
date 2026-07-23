d68e59bd5b14280301c9a09671922fa7208698e6
Iaskasan <nokiesan@yahoo.fr>
2026-07-23 14:22:14 +0200

    fix: reorder import statements for consistency

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Concatenates timestamp-indexed cryptocurrency data."""
-import pandas as pd
 index = __import__("10-index").index
+import pandas as pd
 
 
 def concat(df1, df2):
