929fe80bf657249945a28c413a3fa75a768c31de
Iaskasan <nokiesan@yahoo.fr>
2026-07-23 13:14:51 +0200

    add: add docstring to prune function for clarity

--- pipeline/pandas/8-prune.py
+++ pipeline/pandas/8-prune.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Removes any entries where Close has NaN values."""
+
+
+def prune(df):
+    """Return the DataFrame without rows containing NaN in Close."""
+    return df.dropna(subset=["Close"])
