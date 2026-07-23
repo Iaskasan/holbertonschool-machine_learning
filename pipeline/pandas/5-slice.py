cfce958c69f49beb4c43cab19948660599d4881c
Iaskasan <nokiesan@yahoo.fr>
2026-07-23 11:39:03 +0200

    add: enhance slice function to include docstring and improve readability

--- pipeline/pandas/5-slice.py
+++ pipeline/pandas/5-slice.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Extracts a slice of a DataFrame."""
+
+
+def slice(df):
+    """Extracts a slice of a DataFrame."""
+    return df[["High", "Low", "Close", "Volume_(BTC)"]].iloc[::60, :]
