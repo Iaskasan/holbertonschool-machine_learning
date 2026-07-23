29ebb8f53c2e6a4b25890c94f8a186ef4267a73a
Iaskasan <nokiesan@yahoo.fr>
2026-07-23 13:03:21 +0200

    add: add docstring to high function for clarity

--- pipeline/pandas/7-high.py
+++ pipeline/pandas/7-high.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+"""Sorts a Dataframe by price in descending order"""
+
+
+def high(df):
+    """Sorts a Dataframe by price in descending order"""
+    return df.sort_values(by="High", ascending=False)
