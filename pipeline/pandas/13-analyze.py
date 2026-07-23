9bf41fded71d0a72ccf8e109aa4944c4e005ba15
Iaskasan <nokiesan@yahoo.fr>
2026-07-23 14:48:14 +0200

    add: enhance analyze function to include descriptive statistics computation

--- pipeline/pandas/13-analyze.py
+++ pipeline/pandas/13-analyze.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""Computes descriptive statistics of cryptocurrency data."""
+
+
+def analyze(df):
+    """Computes descriptive statistics of cryptocurrency data."""
+    df_dropped = df.drop(columns=["Timestamp"])
+    return df_dropped.describe()
