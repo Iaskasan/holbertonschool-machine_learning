0742e10205dc51a1094310387bb005363d135905
Iaskasan <nokiesan@yahoo.fr>
2026-07-23 12:55:44 +0200

    add: add docstring to flip_switch function for clarity

--- pipeline/pandas/6-flip_switch.py
+++ pipeline/pandas/6-flip_switch.py
@@ -1,1 +1,8 @@
 #!/usr/bin/env python3
+"""sorts the data in reverse chronological order and transposes it"""
+
+
+def flip_switch(df):
+    """sorts the data in reverse chronological order and transposes it"""
+    df = df.sort_values(by="Timestamp", ascending=False)
+    return df.T
