6cf903d08cd33f3c40427ea2cafcb4c1538b5258
Iaskasan <nokiesan@yahoo.fr>
2026-07-23 11:01:24 +0200

    refactor: revert DataFrame creation to use lists instead of pd.Categorical

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -3,6 +3,7 @@
 import pandas as pd
 
 
-df = pd.DataFrame({'First': pd.Categorical([0.0, 0.5, 1.0, 1.5]),
-                   'Second': pd.Categorical(["one", "two", "three", "four"])},
-                  index=pd.Categorical(['A', 'B', 'C', 'D']))
+df = pd.DataFrame({
+    "First": [0.0, 0.5, 1.0, 1.5],
+    "Second": ["one", "two", "three", "four"],
+}, index=["A", "B", "C", "D"])
