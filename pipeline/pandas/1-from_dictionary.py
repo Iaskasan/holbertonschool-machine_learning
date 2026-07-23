672b40446963d3a76ec97c687d822549c106d08c
Iaskasan <nokiesan@yahoo.fr>
2026-07-23 10:56:37 +0200

    refactor: remove unused numpy import in 1-from_dictionary.py

--- pipeline/pandas/1-from_dictionary.py
+++ pipeline/pandas/1-from_dictionary.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """creates a pd.DataFrame from a dictionary"""
 import pandas as pd
-import numpy as np
 
 
 df = pd.DataFrame({'First': pd.Categorical([0.0, 0.5, 1.0, 1.5]),
